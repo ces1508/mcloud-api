10d0708a542da8d7a16128090caa197d506d6927
christian segura <ces1508@gmail.com>
2016-11-17 16:02:56 -0500

    configure events hook

--- hook.js
+++ hook.js
@@ -17,7 +17,6 @@ if (env === 'test') {
 
 hash.set('POST /create', async function create (req, res, params) {
   let data = await json(req)
-  campaingId
   try {
     await db.connect()
     for (let i = 0; i < data.length; i++) {
