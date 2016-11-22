3fd546bb709968b97f6dc3666239317deb0926ee
christian segura <ces1508@gmail.com>
2016-11-22 15:08:18 -0500

    reports emails

--- reports.js
+++ reports.js
@@ -13,6 +13,52 @@ if (env === 'test') {
   db = new DbStub()
 }
 
+hash.set('POST /report-index', async function reportIndex (req, res , params) {
+  let data = await json(req)
+  try {
+    await db.connect()
+    let token = await utils.extractToken(req)
+    let user = await utils.verifyToken(token, config.secret)
+    let dataSms = []
+    let dataEmail = []
+    if (data.type === 'week') {
+      for (let i = 0; i <= 6; i++) {
+        let fech = new Date(data.date)
+        fech = fech.setDate(fech.getDate() - i)
+        fech = new Date(fech)
+        let day = fech.getDate()
+        let month = fech.getMonth()
+        let year = fech.getFullYear()
+        let midnight = Math.round(new Date(year, month, day, 23, 59, 59).getTime() /1000.0)
+        let dawn = Math.round(new Date(year, month, day).getTime() /1000.0 )
+        dataSms.push(db.getReportSmsByDay(user.id, dawn, midnight))
+        dataEmail.push(db.getReportEmailByDay(user.id, dawn, midnight))
+      }
+    }
+
+    if (data.type === 'month') {
+       for (let i = 0; i <= 28; i++) {
+        let fech = new Date(data.date)
+        fech = fech.setDate(fech.getDate() - i)
+        fech = new Date(fech)
+        let day = fech.getDate()
+        let month = fech.getMonth()
+        let year = fech.getFullYear()
+        let midnight = Math.round(new Date(year, month, day, 23, 59, 59).getTime() /1000.0)
+        let dawn = Math.round(new Date(year, month, day).getTime() /1000.0 )
+        dataSms.push(db.getReportSmsByDay(user.id, dawn, midnight))
+        dataEmail.push(db.getReportEmailByDay(user.id, dawn, midnight))
+      }
+    }
+    let sms = await Promise.all(dataSms)
+    let email = await Promise.all(dataEmail)
+    send(res, 200, {email: email, sms: sms})
+  }
+  catch (e) {
+    console.error(e.message)
+    return send(res, 500,)
+  }
+})
 hash.set('POST /:id/sms', async function find (req, res, params) {
   let id = params.id
   await db.connect()
@@ -61,35 +107,6 @@ hash.set('POST /report-week-sms', async function rWeek (req, res, params) {
     return send(res, 500, e.message)
   }
 })
-hash.set('POST /report-index', async function rIndex (req, res , params) {
-  let date = await json(req)
-  try {
-    await db.connect()
-    let token = await utils.extractToken(req)
-    let user = await utils.verifyToken(token, config.secret)
-    let dataSms = []
-    let dataEmail = []
-     for (let i = 0; i <= 6; i++) {
-      let fech = new Date(date.date)
-      fech = fech.setDate(fech.getDate() - i)
-      fech = new Date(fech)
-      let day = fech.getDate()
-      let month = fech.getMonth()
-      let year = fech.getFullYear()
-      let midnight = Math.round(new Date(year, month, day, 23, 59, 59).getTime() /1000.0)
-      let dawn = Math.round(new Date(year, month, day).getTime() /1000.0 )
-      dataSms.push(db.getReportSmsByDay(user.id, dawn, midnight))
-      dataEmail.push(db.getReportEmailByDay(user.id, dawn, midnight))
-    }
-    let sms = await Promise.all(dataSms)
-    let email = await Promise.all(dataEmail)
-    send(res, 200, {email: email, sms: sms})
-  }
-  catch (e) {
-    console.error(e.message)
-    return send(res, 500,)
-  }
-})
 hash.set('POST /report-by-month', async function reportMonth (req, res, params) {
   let date = await json(req)
   try {
@@ -110,6 +127,79 @@ hash.set('POST /report-by-month', async function reportMonth (req, res, params) 
     }
     let sms = await Promise.all(dataSms)
     send(res, 200, sms)
+  }
+  catch (e) {
+    console.error(e.message)
+    return send(res, 500,)
+  }
+})
+hash.set('GET /:id/email', async function reportByCampaignEmail (req, res, params) {
+  let id = params.id
+  await db.connect()
+  try {
+    let token = await utils.extractToken(req)
+    let user = await utils.verifyToken(token, config.secret)
+    let campaing = await db.find('campaingEmails', id)
+    await utils.checkUser(user, campaing)
+  }
+  catch (e) {
+    console.error(e.message)
+    return send(res, 401, 'unAuthorized')
+  }
+  try {
+    let data = await db.graphEmailsbyCampaign(id)
+    await db.disconnet()
+    send(res, 200, data)
+  } catch (e) {
+    return send(res, 500, {error: e.message})
+  }
+})
+hash.set('POST /reportEmail-by-month', async function reportEmailMonth (req, res, params) {
+  let date = await json(req)
+  try {
+    await db.connect()
+    let token = await utils.extractToken(req)
+    let user = await utils.verifyToken(token, config.secret)
+    let dataEmail = []
+     for (let i = 0; i <= 28; i++) {
+      let fech = new Date(date.date)
+      fech = fech.setDate(fech.getDate() - i)
+      fech = new Date(fech)
+      let day = fech.getDate()
+      let month = fech.getMonth()
+      let year = fech.getFullYear()
+      let midnight = Math.round(new Date(year, month, day, 23, 59, 59).getTime() /1000.0)
+      let dawn = Math.round(new Date(year, month, day).getTime() /1000.0 )
+      dataEmail.push(db.getReportEmailByDay(user.id, dawn, midnight))
+    }
+    let emails = await Promise.all(dataEmail)
+    send(res, 200, emails)
+  }
+  catch (e) {
+    console.error(e.message)
+    return send(res, 500,)
+  }
+})
+hash.set('POST /reportEmail-by-week', async function reportEmailWeek (req, res, params) {
+  let date = await json(req)
+  try {
+    await db.connect()
+    let token = await utils.extractToken(req)
+    let user = await utils.verifyToken(token, config.secret)
+    let dataEmail = []
+     for (let i = 0; i <= 6; i++) {
+      let fech = new Date(date.date)
+      fech = fech.setDate(fech.getDate() - i)
+      fech = new Date(fech)
+      let day = fech.getDate()
+      let month = fech.getMonth()
+      let year = fech.getFullYear()
+      let midnight = Math.round(new Date(year, month, day, 23, 59, 59).getTime() /1000.0)
+      let dawn = Math.round(new Date(year, month, day).getTime() /1000.0 )
+      dataEmail.push(db.getReportEmailByDay(user.id, dawn, midnight))
+    }
+    let emails = await Promise.all(dataEmail)
+    send(res, 200, emails)
   }
   catch (e) {
     console.error(e.message)
