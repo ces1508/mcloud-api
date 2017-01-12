666aed8944260b40c8ebceec67ea036e59da1a20
christian segura <ces1508@gmail.com>
2017-01-12 17:00:37 -0500

    pool connection hook

--- hook.js
+++ hook.js
@@ -9,17 +9,19 @@ import utils from './lib/utils'
 import request from 'request-promise'
 import uuid from 'uuid-base62'
 import sleep from 'then-sleep'
+const r = require('rethinkdbdash')({host: '104.131.43.30',port: 28015})
 // const env = process.env.NODE_ENV || 'test'
 const env = 'production'
 const hash = httpHash()
-let db = new Db(config.db)
-
+// let db = new Db(config.db)
+const events = ['', 'click', 'open', 'bounce', 'dropped', 'spamreport']
+const ramdom = function () {
+  Math.floor((Math.random() * 5) + 1)
+  return events[Math.floor((Math.random() * 5) + 1)]
+}
 hash.set('POST /create', async function create (req, res, params) {
   let data = await json(req)
   try {
-    await sleep(10)
-    await db.connect()
-    let promises = []
     for (let i = 0; i < data.length; i++) {
       if (data[i].campaingId) {
         let newData = {
@@ -27,18 +29,28 @@ hash.set('POST /create', async function create (req, res, params) {
           event: data[i].event,
           email: data[i].email,
           date: data[i].timestamp,
-          userId: data[i].userId
         }
-        promises.push(db.createHook(newData))
+        let campaign = await r.db('mepscloud').table('campaingEmails').get(newData.campaingId)
+        if (campaign) {
+          newData.userId = campaign.userId
+          let old_data = await r.db('mepscloud').table('historicEmail').getAll(data[i].email, {index: 'email'})
+          .filter({campaingId: data[i].campaingId})
+          console.log(old_data)
+          if (newData.event === 'open'  && old_data[0].event != 'click') {
+            await r.db('mepscloud').table('historicEmail').getAll(data[i].email, {index: 'email'})
+            .filter({campaingId: newData.campaingId}).update({date: newData.date})
+          } else {
+             await r.db('mepscloud').table('historicEmail').getAll(data[i].email, {index: 'email'})
+            .filter({campaingId: newData.campaingId}).update({event: newData.event, date: newData.date})
+          }
+        }
       }
     }
-    await Promise.all(promises)
   } catch (e) {
     console.error(e.message)
     return send(res, 500, {error: e.message})
   }
 })
-//test optimzate hook
 
 hash.set('GET /', async function get (req, res, params) {
   try {
@@ -47,49 +59,10 @@ hash.set('GET /', async function get (req, res, params) {
     console.error(e.message)
   }
 })
-hash.set('POST /', async function testPost (req, res, params) {
- let options = {
-  method: 'POST',
-  uri: 'https://gateway.plusmms.net/rest/message',
-  headers: {
-    'authorization': 'Basic bWVwc2Nsb3VkY286TUVQJCQ5MDA=ad'
-  },
-    resolveWithFullResponse: true,
-    json: true,
-    body: {
-      to: ['573203230522'],
-      text: 'asdasdasdasd adsads',
-      tresc: 'true',
-      from: '3203230522'
-    }
-  }
- // try {
-  let history = []
-  for (let i = 0; i <= 1; i++) {
-    try {
-      await request(options)
-      history.push({
-        number: options.body.to[0],
-        text: options.body.text,
-        status: 'send'
-      })
-    } catch (e) {
-      history.push({
-        number: options.body.to[0],
-        text: options.body.text,
-        status: 'failed'
-      })
-    }
-  }
-   return send(res, 201,history)
- // } catch (e) {
-   // console.log(e.message)
-   // return send(res, 200, `error: ${e.message}`)
- // }
-})
-// hash.set('GET /loaderio-aea8ef80676eb14bca4c66b0f8397353', async function loaderio (req, res, params) {
-//   send(res, 200, 'loaderio-aea8ef80676eb14bca4c66b0f8397353')
-// })
+//  hash.set('GET /loaderio-aea8ef80676eb14bca4c66b0f8397353', async function loaderio (req, res, params) {
+//    send(res, 200, 'loaderio-aea8ef80676eb14bca4c66b0f8397353')
+
+//  })
 export default async function main (req, res) {
   let  { method, url } = req
   let match = hash.get(`${method.toUpperCase()} ${url}`)
